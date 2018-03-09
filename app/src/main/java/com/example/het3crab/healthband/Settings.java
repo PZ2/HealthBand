cc2b088b77c0a156d3f792245e1a5d8a27103c44
Arkadiusz Wolk - ArcziPT <arek.w999@gmail.com>
2018-03-09 21:13:28 +0100

    settings saving

--- app/src/main/java/com/example/het3crab/healthband/Settings.java
+++ app/src/main/java/com/example/het3crab/healthband/Settings.java
@@ -1,6 +1,8 @@
 package com.example.het3crab.healthband;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +10,14 @@ import android.widget.Button;
 import android.widget.ToggleButton;
 
 public class Settings extends AppCompatActivity {
-    private Button toMainButton;
+    private final String APP = "com.example.het3crab.healthband";
+    private final String ALERTS_KEY = "com.example.het3crab.healthband.alerts";
+    private final String PULSE_FREQ_KEY = "com.example.het3crab.healthband.pulsefreq";
+
+    private boolean isAlertOn = false;
+    private int pulseFreq = 60;
+
+    private Button saveButton;
     private ToggleButton alerts;
 
     @Override
@@ -17,11 +26,11 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         alerts = (ToggleButton) findViewById(R.id.alerts);
-        toMainButton = (Button) findViewById(R.id.toMainButton);
-        toMainButton.setOnClickListener(new View.OnClickListener() {
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toMain();
+                saveSettings();
             }
         });
         alerts.setOnClickListener(new View.OnClickListener() {
@@ -30,14 +39,30 @@ public class Settings extends AppCompatActivity {
                 alerts();
             }
         });
+
+        readSettings();
     }
 
-    public void toMain() {
+    private void readSettings(){
+        SharedPreferences prefs = this.getSharedPreferences(
+                APP , Context.MODE_PRIVATE);
+
+        isAlertOn = prefs.getBoolean(ALERTS_KEY, false);
+        pulseFreq = prefs.getInt(PULSE_FREQ_KEY, 60);
+    }
+
+    private void saveSettings() {
+        SharedPreferences prefs = this.getSharedPreferences(
+                APP , Context.MODE_PRIVATE);
+
+        prefs.edit().putBoolean(APP, isAlertOn).apply();
+        prefs.edit().putInt(APP, pulseFreq).apply();
+
         Intent Main = new Intent(this, Main.class);
         startActivity(Main);
     }
 
-    public void alerts() {
+    private void alerts() {
 
     }
 
